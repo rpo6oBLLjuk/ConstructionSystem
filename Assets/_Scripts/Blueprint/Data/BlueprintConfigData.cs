765e59afc428a7fb723b9c8ad3c65164fd5af62f
Гробовщик <128229502+rpo6oBLLjuk@users.noreply.github.com>
2025-10-31 15:37:39 +0300

    .

---
+++ Assets/_Scripts/Blueprint/Data/BlueprintConfigData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BlueprintSystemConfig", menuName = "Scriptable Objects/Blueprint/SystemConfig")]
+public class BlueprintSystemConfig : ScriptableObject 
+{
+    
+}
---
+++ Assets/_Scripts/Extensions/TMP/ExtendedTMP_Dropdown.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExtendedTMP_Dropdown : TMP_Dropdown
+{
+    //[SerializeField]
+    //private bool m_HideOnSelect = true;
+
+
+    //override OnSelectItem()
+    //{
+
+    //}
+    //public override void OnSelect(BaseEventData eventData)
+    //{
+    //    base.OnSelect(eventData);
+    //}
+}
